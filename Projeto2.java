eadf8867356e96d242c0d78973e5065abd302395
Jotave <joaovmmartins21@gmail.com>
2025-07-29 20:45:30 -0300

    Add files via upload

---
+++ Projeto2.java
@@ -0,0 +1,17 @@
+
+package projeto2;
+public class Projeto2 {
+    public static void main(String[] args) {
+        
+        int x;
+        x = 0;
+        
+        while (x < 10){
+            System.out.println("X vale: " + x);
+            
+            x = x + 1;
+        }
+        
+    }
+    
+}
---
+++ Projeto5.java
@@ -0,0 +1,11 @@
+package projeto5;
+public class Projeto5 {
+    public static void main(String[] args) {
+
+        String texto;
+        texto = "Hello World";
+        System.out.println(texto);
+        
+    }
+    
+}
