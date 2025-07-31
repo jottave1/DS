659fc4164aea12c6c6f4288c05ae7a7a1eff7ef5
Jotave <joaovmmartins21@gmail.com>
2025-07-31 00:55:19 -0300

    Add files via upload

---
+++ DS I/Java/Projeto4.java
@@ -0,0 +1,13 @@
+package projeto4;
+public class Projeto4 {
+    public static void main(String[] args) {
+        
+        int x;
+        x = 0;
+        do {
+       
+            System.out.println("X vale: " + x);
+            x = x + 1;
+        }   while ( x < 10);              
+    }
+}
