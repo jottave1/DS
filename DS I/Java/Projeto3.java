1cb44a8150b5637ad5f5f5471c2b5804c1967b60
Jotave <joaovmmartins21@gmail.com>
2025-07-31 00:53:32 -0300

    Add files via upload

---
+++ DS I/Java/Projeto3.java
@@ -0,0 +1,14 @@
+package projeto3;
+public class Projeto3 {
+    public static void main(String[] args) {
+        
+        int x;
+        
+        for (x = 0; x < 10; x = x + 1){
+            
+            System.out.println("X vale: " + x);
+        
+    }
+    
+}
+}   
