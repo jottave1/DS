a5ced1f466f8c4965eee650b8979795848d2c5a8
Jotave <joaovmmartins21@gmail.com>
2025-07-31 00:36:50 -0300

    Add files via upload

---
+++ DS I/DS I/Projeto6.java
@@ -0,0 +1,13 @@
+package projeto6;
+public class Projeto6 {
+    public static void main(String[] args) {
+        
+        String texto;
+        int tamanho;
+        texto = "Hello World";
+        tamanho = texto.length();
+        System.out.println("O texto tem " + tamanho + " letras. ");
+        
+    }
+    
+}
